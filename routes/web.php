789b9f793b7514dc04f4d25b66a53d7657102bec
ArthurIf <aheof@discente.ifpe.edu.br>
2022-10-11 19:50:34 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/livro/novo','LivrosController@create');
+Route::post('/livro/novo','LivrosController@store')->name('salvar_livro');
+Route::get('/livros/ver','LivrosController@show');
+Route::get('/livro/del/{id}','LivrosController@destroy')->name('excluir_livro');
+Route::get('/livro/edit/{id}','LivrosController@edit')->name('editar_livro');
+Route::post('/livro/edit/{id}','LivrosController@update')->name('atualizar_livro');
+
